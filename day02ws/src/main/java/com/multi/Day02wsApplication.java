348451ae0a39630f64488c40ba00bce960ae9bf6
noranbear <norandoly@gmail.com>
2022-05-11 18:31:05 +0900

    2022-05-11 Workshop-다이나믹 웹페이지 기본 연습

---
+++ day02ws/src/main/java/com/multi/Day02wsApplication.java
@@ -0,0 +1,13 @@
+package com.multi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day02wsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day02wsApplication.class, args);
+	}
+
+}
